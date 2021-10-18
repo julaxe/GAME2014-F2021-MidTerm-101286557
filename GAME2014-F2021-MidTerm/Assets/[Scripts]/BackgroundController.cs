29c1553140416894b395fddd81c9f76e062b6c5a
Julaxe <julaxe8@gmail.com>
2021-10-18 11:21:51 -0400

    background scroll horizontally

--- GAME2014-F2021-MidTerm/Assets/[Scripts]/BackgroundController.cs
+++ GAME2014-F2021-MidTerm/Assets/[Scripts]/BackgroundController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +16,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
